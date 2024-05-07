332ed9c8303b2a834be404c48314e16f3a901f73
majamarijan <majapianist@gmail.com>
2024-05-07 16:10:58 +0200

    add: theme button

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,7 @@
 window.onload = () => {
 	const form = document.querySelector("form") || null;
+	const themeBtn = document.querySelector("input[type='checkbox']");
+
 	if (form) {
 		let data = {};
 		form.onsubmit = (e) => {
@@ -20,4 +22,22 @@ window.onload = () => {
 			});
 		};
 	}
+	if (window.localStorage.getItem("theme")) {
+		let theme = window.localStorage.getItem("theme");
+		document.body.classList.add(theme);
+		themeBtn.checked = true;
+	}
+	themeController(themeBtn);
 };
+
+function themeController(themeBtn) {
+	themeBtn.addEventListener("change", (e) => {
+		if (themeBtn.checked) {
+			document.body.classList.add("dark");
+			window.localStorage.setItem("theme", "dark");
+		} else {
+			document.body.classList.remove("dark");
+			window.localStorage.removeItem("theme");
+		}
+	});
+}
